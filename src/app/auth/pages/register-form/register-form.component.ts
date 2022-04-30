9c1332824806ed2f495a8648d06a5aee189d475a
ivan-nesusanin <l75x5@yandex.ru>
2022-04-30 17:02:06 +0300

    feat: add pattern validator in the login-form

--- src/app/auth/pages/register-form/register-form.component.ts
+++ src/app/auth/pages/register-form/register-form.component.ts
@@ -24,6 +24,7 @@ export class RegisterFormComponent implements OnInit {
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(8),
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])'),
         this.validatorOfRegister,
         this.validatorOfNumber,
         this.validatorOfSymbol,
